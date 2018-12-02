91ab7b41a211d559700a74f061a00b8c040c9eee
James Orr <wwwmailacct@gmail.com>
2018-12-02 12:16:05 -0500

    prettier.

--- Day02/Day02.1.py
+++ Day02/Day02.1.py
@@ -1,11 +1,8 @@
 with open('input','r') as f: input = f.read() 
 ids = input.split('\n')
 
-(twos, threes) = (0,0)
-
-for id in ids:
-    counts = {char : id.count(char) for char in set(id)}
-    if any(x == 2 for x in counts.values()): twos += 1
-    if any(x == 3 for x in counts.values()): threes += 1  
+counts = [{id.count(char) for char in set(id)} for id in ids]
+twos = sum(2 in x for x in counts)
+threes = sum(3 in x for x in counts)
 
 print twos * threes
