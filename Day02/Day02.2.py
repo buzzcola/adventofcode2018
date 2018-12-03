a845248ef7ad24b16622cd7ceef219cabb04e6bd
James Orr <wwwmailacct@gmail.com>
2018-12-03 18:52:03 -0500

    prettier.

--- Day02/Day02.2.py
+++ Day02/Day02.2.py
@@ -2,8 +2,8 @@ import itertools
 with open('input','r') as f: input = f.read() 
 ids = input.split('\n')
 
-for pair in itertools.product(ids, ids):
-    result = ''.join(['' if x <> y else x for (x,y) in zip(pair[0],pair[1])])
-    if len(result) == len(pair[0]) - 1:
+for left,right in itertools.product(ids, ids):
+    result = ''.join(['' if c1 <> c2 else c1 for (c1,c2) in zip(left, right)])
+    if len(result) == len(left) - 1:
         print result
         break
