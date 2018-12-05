753f40394901a7ae6e78a93eb8087694156f67c4
James Orr <jorr@questica.com>
2018-12-05 09:26:27 -0500

    product -> combinations!

--- Day02/Day02.2.py
+++ Day02/Day02.2.py
@@ -2,7 +2,7 @@ import itertools
 with open('input','r') as f: input = f.read() 
 ids = input.split('\n')
 
-for left,right in itertools.product(ids, ids):
+for left,right in itertools.combinations(ids, 2):
     result = ''.join(['' if c1 <> c2 else c1 for (c1,c2) in zip(left, right)])
     if len(result) == len(left) - 1:
         print result
