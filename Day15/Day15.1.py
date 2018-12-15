d67f69de677ea91f4a7c07e027c643a2e3dabd81
buzzcola <orr.james@gmail.com>
2018-12-15 16:00:45 -0500

    WIP

--- Day15/Day15.1.py
+++ Day15/Day15.1.py
@@ -1,4 +1,4 @@
-from Shared import Goblin, Elf, print_grid
+from Shared import Goblin, Elf, Fighter, print_grid, find_enemies
 
 with open('input','r') as f: input = f.read()
 (ELF, GOBLIN, WALL) = ['E','G','#']
@@ -10,3 +10,36 @@ for y, row in zip(range(len(rows)), rows):
         elif char == GOBLIN: grid[(x,y)] = Goblin()
         elif char == WALL: grid[(x,y)] = WALL
 
+round = 0
+while True:    
+    goblins_remain = any(type(x) is Goblin for x in grid.values())
+    elves_remain = any(type(x) is Elf for x in grid.values())
+    if not goblins_remain or not elves_remain:
+        break
+    
+    round += 1
+    fighters = [(coord,x) for coord,x in grid.items() if isinstance(x, Fighter)]
+    fighters.sort(key=lambda x:x[0]) # sort by coordinates to ensure reading order
+    for (location,fighter) in fighters:
+        enemy_nodes = find_enemies(grid, location, fighter.enemy)
+        if not any(enemy_nodes): continue
+        adjacent_enemies = [(x.location, grid[x.location]) for x in enemy_nodes if x.depth == 1]
+
+        # move towards enemy
+        if not any(adjacent_enemies):
+            target_distance = enemy_nodes.min(key=lambda x:x.depth)
+            targets = [x for x in enemy_nodes if x.depth == target_distance]
+            next_location = min(x.ancestors[x.depth - 1].location for x in targets)
+            del grid[location]
+            grid[next_location] = fighter
+            enemy_nodes = find_enemies(grid, coord, fighter.enemy)
+            adjacent_enemies = [(x.location, grid[x.location]) for x in enemy_nodes if x.depth == 1]
+        
+        # attack!
+        if any(adjacent_enemies):
+            adjacent_enemies.sort(key=lambda x:(x[1].hitpoints, x[0]))
+            victim = adjacent_enemies[0]
+            victim[1].hitpoints -= fighter.attack_power
+            if victim[1].hitpoints <= 0:
+                # kill!
+                del grid[victim[0]]
--- Day15/Shared.py
+++ Day15/Shared.py
@@ -1,37 +1,10 @@
 from __future__ import print_function
-from anytree import AnyNode
+from anytree import AnyNode, RenderTree
 
-class Fighter:
+class Fighter(object):
     def __init__(self):
         self.hitpoints = 200
-        self.attack = 3
-
-    def take_turn(self):
-        # search for enemies
-        
-
-class BreadthFirstSearch:
-    def __init__(self, grid, start, goal):
-        self.grid = grid
-        self.start = start
-        self.goal = goal
-        self.visited = set()
-        self.enemy_nodes = []
-        self.search(self.start)
-
-    def search(self, coord):
-        node = AnyNode(location=coord)
-        adjacent = [(coord[0] + d[0], coord[1] + d[1]) for d in Direction.ALL]
-        new = [(x,y) for x,y in adjacent if (x,y) not in self.visited]
-        empty = [(x,y) for x,y in new if (x,y) not in self.grid]
-        enemies = [(x,y) for x,y in new if self.grid[(x,y)] == self.goal]
-        self.enemy_nodes += [AnyNode(location = (x,y), parent=node) for x,y in enemies]
-        self.visited.update([coord] + enemies)
-        if any(self.enemy_nodes):
-            return
-        for x in empty:
-            self.search(x)
-
+        self.attack_power = 3
 
 class Elf(Fighter):
     def __init__(self):
@@ -46,6 +19,23 @@ class Goblin(Fighter):
         self.enemy = 'E'
 
     def __str__(self): return 'G'
+
+def find_enemies(grid, start, goal):
+    start_node = AnyNode(location=start)
+    frontier = [start_node]
+    visited = set()
+    enemy_nodes = []
+    while any(frontier):
+        node = frontier.pop(0)
+        coord = node.location
+        adjacent = [(coord[0] + d[0], coord[1] + d[1]) for d in Direction.ALL]            
+        enemies = [(x,y) for x,y in adjacent if grid.get((x,y), None) == goal]
+        next_nodes = [AnyNode(location=(x,y), parent=node) for x,y in adjacent if (x,y) not in grid and (x,y) not in visited]
+        enemy_nodes += [AnyNode(location = (x,y), parent=node) for x,y in enemies]
+        visited.update([coord])
+        frontier += next_nodes
+    
+    return enemy_nodes
 
 class Direction:
     NORTH = (0,-1)
