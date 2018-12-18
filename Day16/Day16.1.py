f5f0b42de347b2a3ed8b2d8d00b13df87e447690
buzzcola <orr.james@gmail.com>
2018-12-18 17:50:33 -0500

    prettier.

--- Day16/Day16.1.py
+++ Day16/Day16.1.py
@@ -4,11 +4,9 @@ import operator
 with open('input','r') as f: input = f.read()
 first_half = input.split('\n\n\n\n')[0]
 tests = [Test(x) for x in first_half.split('\n\n')]
-
 machine = Machine()
-
-
 three_plus_tests = 0
+
 for test in tests:
     passing = 0
     for op in machine.opcodes():
@@ -20,12 +18,3 @@ for test in tests:
             break
 
 print three_plus_tests
-
-'''
-for op in machine.opcodes():
-    i = Instruction(op, 2, 1, 2)
-    machine.registers = [3, 2, 1, 1]
-    machine.execute(i)
-    if machine.registers == [3,2,2,1]:
-        print '%s: %s' % (op, machine.registers)
-'''
